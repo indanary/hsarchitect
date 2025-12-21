055ebc2127f3cce15e227bc1c6b28da1a348022f
indanary <indanary@gmail.com>
2025-12-21 17:29:26 +0700

    add 404

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,21 @@
 // tailwind.config.js
 export default {
-  content: [
-    './src/**/*.{astro,html,js,jsx,ts,tsx}', // Include React components
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+	content: [
+		"./src/**/*.{astro,html,js,jsx,ts,tsx}", // Include React components
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+	safelist: [
+		"min-h-screen",
+		"tracking-widest",
+		"underline-offset-4",
+		"opacity-60",
+		"opacity-70",
+		"text-xs",
+		"text-sm",
+		"text-2xl",
+		"text-3xl",
+	],
+}
